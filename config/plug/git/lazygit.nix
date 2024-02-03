7e3c62f2b7caeacfb5d00c875eeff1a52d39c3c9
elythh <gwenchlan@tuta.io>
2024-02-03 03:57:21 +0100

    refact(plugs)

---
+++ config/plug/git/lazygit.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  extraPlugins = with pkgs.vimPlugins; [
+    lazygit-nvim
+  ];
+
+  extraConfigLua = ''
+    require("telescope").load_extension("lazygit")
+  '';
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>gg";
+      action = "<cmd>LazyGit<CR>";
+      options = {
+        desc = "LazyGit (root dir)";
+      };
+    }
+  ];
+}
