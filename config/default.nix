b12baf5053f28e9875c051ab79a3ca1ef5aa3ede
elythh <gwenchlan@tuta.io>
2024-02-03 04:19:40 +0100

    feat(colorscheme)

--- config/default.nix
+++ config/default.nix
@@ -6,6 +6,7 @@
 
     ./plug/utils/oil.nix
     ./plug/utils/whichkey.nix
+    ./plug/colorscheme/colorscheme.nix
     ./plug/ui/telescope.nix
     ./plug/ui/lualine.nix
     ./plug/git/lazygit.nix
---
+++ config/plug/colorscheme/colorscheme.nix
@@ -0,0 +1,52 @@
+{
+  colorschemes = {
+    rose-pine = {
+      enable = false;
+      style = "moon"; #  "main", "moon", "dawn" or raw lua code
+      disableItalics = false;
+      transparentFloat = true;
+      transparentBackground = true;
+    };
+    catppuccin = {
+      enable = true;
+      background = {
+        light = "macchiato";
+        dark = "mocha";
+      };
+      flavour = "mocha"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
+      disableBold = false;
+      disableItalic = false;
+      disableUnderline = false;
+      transparentBackground = true;
+      integrations = {
+        cmp = true;
+        noice = true;
+        notify = true;
+        neotree = true;
+        harpoon = true;
+        gitsigns = true;
+        which_key = true;
+        illuminate = {
+          enabled = true;
+        };
+        treesitter = true;
+        treesitter_context = true;
+        telescope.enabled = true;
+        indent_blankline.enabled = true;
+        mini.enabled = true;
+        native_lsp = {
+          enabled = true;
+          inlay_hints = {
+            background = true;
+          };
+          underlines = {
+            errors = [ "underline" ];
+            hints = [ "underline" ];
+            information = [ "underline" ];
+            warnings = [ "underline" ];
+          };
+        };
+      };
+    };
+  };
+}
