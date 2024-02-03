d446b3163f5e08ccc18a05a2c2f40c5ac6611b7a
elythh <gwenchlan@tuta.io>
2024-02-03 06:48:34 +0100

    :wrench: core: re-order imports

--- config/default.nix
+++ config/default.nix
@@ -4,21 +4,27 @@
     ./keys.nix
     ./sets.nix
 
-    ./plug/lsp/none-ls.nix
-    ./plug/lsp/lsp.nix
-    ./plug/snippets/luasnip.nix
-    ./plug/completion/lspkind.nix
+    ./plug/colorscheme/colorscheme.nix
+
     ./plug/completion/cmp.nix
+    ./plug/completion/lspkind.nix
+
+    ./plug/git/gitsigns.nix
+    ./plug/git/lazygit.nix
+
+    ./plug/lsp/lsp.nix
+    ./plug/lsp/none-ls.nix
+
+    ./plug/snippets/luasnip.nix
+
+    ./plug/ui/alpha.nix
+    ./plug/ui/bufferline.nix
+    ./plug/ui/lualine.nix
+    ./plug/ui/noice.nix
+    ./plug/ui/telescope.nix
+
+    ./plug/utils/nvim-autopairs.nix
     ./plug/utils/oil.nix
     ./plug/utils/whichkey.nix
-    ./plug/utils/nvim-autopairs.nix
-    ./plug/colorscheme/colorscheme.nix
-    ./plug/ui/alpha.nix
-    ./plug/ui/noice.nix
-    ./plug/ui/bufferline.nix
-    ./plug/ui/telescope.nix
-    ./plug/ui/lualine.nix
-    ./plug/git/lazygit.nix
-    ./plug/git/gitsigns.nix
   ];
 }
