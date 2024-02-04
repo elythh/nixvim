0e4306c861edfd020b82cd7d19dfe01df7961482
elythh <gwenchlan@tuta.io>
2024-02-04 01:43:33 +0100

    :wrench: keys: move windows

--- config/keys.nix
+++ config/keys.nix
@@ -167,6 +167,46 @@
 
     {
       mode = "n";
+      key = "<C-h>";
+      action = "<C-W>h";
+      options = {
+        silent = true;
+        desc = "Move to window left";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<C-l>";
+      action = "<C-W>l";
+      options = {
+        silent = true;
+        desc = "Move to window right";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<C-k>";
+      action = "<C-W>k";
+      options = {
+        silent = true;
+        desc = "Move to window over";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<C-j>";
+      action = "<C-W>j";
+      options = {
+        silent = true;
+        desc = "Move to window bellow";
+      };
+    }
+
+    {
+      mode = "n";
       key = "<C-s>";
       action = "<cmd>w<cr><esc>";
       options = {
