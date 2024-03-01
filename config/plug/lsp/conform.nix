8e8dbca682e8b04692f13f53ae2b7d3b0b50ebe5
elythh <gwenchlan@tuta.io>
2024-03-01 22:03:27 +0100

    :wrench: (conform): add liquidsoap-prettier
    It's not yet available in the nixpkgs but It's been merged to master on the conform.nvim repo

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -7,6 +7,7 @@
     };
     notifyOnError = true;
     formattersByFt = {
+      liquidsoap = [ "liquidsoap-prettier" ];
       html = [ [ "prettierd" "prettier" ] ];
       css = [ [ "prettierd" "prettier" ] ];
       javascript = [ [ "prettierd" "prettier" ] ];
