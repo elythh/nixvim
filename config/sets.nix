dd8188ea812221ee38f6c01aed7ae72dcf9fc969
elythh <gwenchlan@tuta.io>
2024-03-01 21:01:46 +0100

    :wrench: (cursor): stay default

--- config/sets.nix
+++ config/sets.nix
@@ -79,15 +79,6 @@
       encoding = "utf-8";
       fileencoding = "utf-8";
 
-      # Change cursor options
-      guicursor = [
-        "n-v-c:block" # Normal, visual, command-line: block cursor
-        "i-ci-ve:block" # Insert, command-line insert, visual-exclude: vertical bar cursor with block cursor, use "ver25" for 25% width
-        "r-cr:hor20" # Replace, command-line replace: horizontal bar cursor with 20% height
-        "o:hor50" # Operator-pending: horizontal bar cursor with 50% height
-        "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor" # All modes: blinking settings
-        "sm:block-blinkwait175-blinkoff150-blinkon175" # Showmatch: block cursor with specific blinking settings
-      ];
 
       # More space in the neovim command line for displaying messages
       cmdheight = 0;
