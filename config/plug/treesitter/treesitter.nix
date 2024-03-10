e7fa5767297f5e8190e943088f1bae0baee323d0
elythh <gwen@omg.lol>
2024-03-10 21:30:18 +0100

    :wrench: (treesitter): add Liquidsoap

--- config/plug/treesitter/treesitter.nix
+++ config/plug/treesitter/treesitter.nix
@@ -1,8 +1,21 @@
-{
+{pkgs, ...}: {
   plugins.treesitter = {
     enable = true;
     indent = true;
     folding = true;
     nixvimInjections = true;
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
   };
+  filetype = {
+    extension = {
+      liq = "liquidsoap";
+    };
+  };
+  extraConfigLua = ''
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    parser_config.liquidsoap = {
+      filetype = "liquidsoap",
+    }
+  '';
 }
