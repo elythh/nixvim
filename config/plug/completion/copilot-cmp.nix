40573ec7e34576eae871523d1796553ccfd0f5d5
elythh <gwen@omg.lol>
2024-03-25 15:34:42 +0100

    :bug: (copilot): disable panel and suggestion

--- config/plug/completion/copilot-cmp.nix
+++ config/plug/completion/copilot-cmp.nix
@@ -2,6 +2,11 @@
   plugins.copilot-cmp = {
     enable = true;
   };
+  plugins.copilot-lua = {
+    enable = true;
+    suggestion = {enabled = false;};
+    panel = {enabled = false;};
+  };
 
   extraConfigLua = ''
     require("copilot").setup({
