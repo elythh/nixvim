000a4ebe4e5ee1b219371912fef49a5b7602837b
elythh <gwen@omg.lol>
2024-03-28 19:19:49 +0100

    :wrench: (copilot): add keymaps
    This commit introduces new keymaps for the CopilotChat feature in
    Neovim. The keymaps provide shortcuts for various CopilotChat
    functions such as Explain, Fix, Docs, and Commit. This enhancement
    aims to improve the user experience and efficiency when using
    CopilotChat in Neovim.
    
    This bullshit commit description is written thanks to it also.

--- config/plug/utils/copilot.nix
+++ config/plug/utils/copilot.nix
@@ -25,4 +25,32 @@ in {
   extraConfigLua = ''
     require("CopilotChat").setup { }
   '';
+
+  keymaps = [
+    {
+      mode = "x";
+      key = "<leader>a";
+      action = "+copilot";
+    }
+    {
+      mode = "x";
+      key = "<leader>ae";
+      action = "<cmd>CopilotChatExplain<cr>";
+    }
+    {
+      mode = "x";
+      key = "<leader>af";
+      action = "<cmd>CopilotChatFix<cr>";
+    }
+    {
+      mode = "x";
+      key = "<leader>ad";
+      action = "<cmd>CopilotChatDocs<cr>";
+    }
+    {
+      mode = "x";
+      key = "<leader>ac";
+      action = "<cmd>CopilotChatCommit<cr>";
+    }
+  ];
 }
