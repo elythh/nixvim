4a773be5bc7d991bed704d2359ac134a42d86526
elythh <gwen@omg.lol>
2024-03-31 17:30:12 +0200

    :wrench: (telescope): file browser

--- config/plug/ui/telescope.nix
+++ config/plug/ui/telescope.nix
@@ -2,6 +2,9 @@
   plugins.telescope = {
     enable = true;
     extensions = {
+      file_browser = {
+        enable = true;
+      };
       fzf-native = {
         enable = true;
       };
@@ -124,6 +127,22 @@
       action = "<cmd>Telescope diagnostics bufnr=0<cr>";
       options = {
         desc = "Document diagnostics";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fe";
+      action = "<cmd>Telescope file_browser<cr>";
+      options = {
+        desc = "File browser";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fE";
+      action = "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>";
+      options = {
+        desc = "File browser";
       };
     }
   ];
