4ac4656d13bba4187c3a58336656cac68c716458
elythh <gwen@omg.lol>
2024-04-12 21:48:07 +0200

    :wrench: (colorscheme): make colors more consistant

--- config/default.nix
+++ config/default.nix
@@ -62,6 +62,7 @@
     };
   };
   config = {
+    # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
     theme = "paradise";
     extraConfigLua = ''
       _G.theme = "${config.theme}"
--- config/highlight.nix
+++ config/highlight.nix
@@ -1,115 +1,121 @@
-{config, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   colors = import ../config/colors/${config.theme}.nix {};
 in {
-  highlight = {
-    CursorLine = {
-      fg = "none";
-      bg = "none";
-    };
-    CursorLineNr = {
-      fg = "none";
-      bg = "none";
-    };
+  config = lib.mkIf config.colorschemes.base16.enable {
+    highlight = {
+      CursorLine = {
+        fg = "none";
+        bg = "none";
+      };
+      CursorLineNr = {
+        fg = "none";
+        bg = "none";
+      };
 
-    AlphaHeader = {
-      fg = colors.base0E;
-      bg = "none";
-    };
+      AlphaHeader = {
+        fg = colors.base0E;
+        bg = "none";
+      };
 
-    StatusNormal = {
-      bg = "none";
-      fg = "none";
-    };
+      StatusNormal = {
+        bg = "none";
+        fg = "none";
+      };
 
-    TelescopePromptBorder = {
-      fg = colors.base01;
-      bg = colors.base01;
-    };
-    TelescopePromptNormal = {
-      fg = "none";
-      bg = colors.base01;
-    };
-    TelescopePromptPrefix = {
-      fg = colors.base08;
-      bg = colors.base01;
-    };
+      TelescopePromptBorder = {
+        fg = colors.base01;
+        bg = colors.base01;
+      };
+      TelescopePromptNormal = {
+        fg = "none";
+        bg = colors.base01;
+      };
+      TelescopePromptPrefix = {
+        fg = colors.base08;
+        bg = colors.base01;
+      };
 
-    TelescopeSelection = {
-      fg = "none";
-      bg = colors.base01;
-    };
+      TelescopeSelection = {
+        fg = "none";
+        bg = colors.base01;
+      };
 
-    Pmenu = {
-      fg = "none";
-      bg = colors.base01;
-    };
-    PmenuSbar = {
-      fg = "none";
-      bg = colors.base01;
-    };
-    PmenuThumb = {
-      fg = "none";
-      bg = colors.base01;
-    };
-    PmenuSel = {
-      fg = "none";
-      bg = colors.base02;
-    };
+      Pmenu = {
+        fg = "none";
+        bg = colors.base01;
+      };
+      PmenuSbar = {
+        fg = "none";
+        bg = colors.base01;
+      };
+      PmenuThumb = {
+        fg = "none";
+        bg = colors.base01;
+      };
+      PmenuSel = {
+        fg = "none";
+        bg = colors.base02;
+      };
 
-    CmpItemAbbrMatch = {
-      fg = colors.base05;
-      bg = "none";
-    };
-    CmpItemAbbrMatchFuzzy = {
-      fg = colors.base05;
-      bg = "none";
-    };
-    CmpItemAbbr = {
-      fg = colors.base03;
-      bg = "none";
-    };
-    CmpItemKind = {
-      fg = colors.base0E;
-      bg = "none";
-    };
-    CmpItemMenu = {
-      fg = colors.base0E;
-      bg = "none";
-    };
-    CmpItemKindSnippet = {
-      fg = colors.base0E;
-      bg = "none";
-    };
+      CmpItemAbbrMatch = {
+        fg = colors.base05;
+        bg = "none";
+      };
+      CmpItemAbbrMatchFuzzy = {
+        fg = colors.base05;
+        bg = "none";
+      };
+      CmpItemAbbr = {
+        fg = colors.base03;
+        bg = "none";
+      };
+      CmpItemKind = {
+        fg = colors.base0E;
+        bg = "none";
+      };
+      CmpItemMenu = {
+        fg = colors.base0E;
+        bg = "none";
+      };
+      CmpItemKindSnippet = {
+        fg = colors.base0E;
+        bg = "none";
+      };
 
-    VertSplit = {
-      fg = colors.base01;
-      bg = "none";
-    };
-    FloatBorder = {
-      fg = colors.base01;
-      bg = colors.base01;
-    };
-    NormalFloat = {
-      fg = "none";
-      bg = colors.base01;
-    };
+      VertSplit = {
+        fg = colors.base01;
+        bg = "none";
+      };
+      FloatBorder = {
+        fg = colors.base01;
+        bg = colors.base01;
+      };
+      NormalFloat = {
+        fg = "none";
+        bg = colors.base01;
+      };
 
-    LineNr = {
-      fg = colors.base03;
-      bg = "none";
-    };
+      LineNr = {
+        fg = colors.base03;
+        bg = "none";
+      };
 
-    NoiceCmdlinePopup = {
-      fg = colors.base04;
-      bg = colors.base01;
-    };
-    NoiceCmdlinePopupBorder = {
-      fg = colors.base01;
-      bg = colors.base01;
-    };
-    NoiceCmdlinePopupBorderSearch = {
-      fg = colors.base01;
-      bg = colors.base01;
+      NoiceCmdlinePopup = {
+        fg = colors.base04;
+        bg = colors.base01;
+      };
+      NoiceCmdlinePopupBorder = {
+        fg = colors.base01;
+        bg = colors.base01;
+      };
+      NoiceCmdlinePopupBorderSearch = {
+        fg = colors.base01;
+        bg = colors.base01;
+      };
     };
   };
 }
