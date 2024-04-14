9a8d33da1f4e638fb76dab358cc57b3d0c2258b8
elythh <gwen@omg.lol>
2024-04-14 14:41:26 +0200

    :lipstick: (colorscheme): everblush

--- config/default.nix
+++ config/default.nix
@@ -63,7 +63,7 @@
   };
   config = {
     # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
-    theme = "paradise";
+    theme = "everblush";
     extraConfigLua = ''
       _G.theme = "${config.theme}"
     '';
