d382ef1ef0ff5b01a1b95fd31d3888018588abd1
Rajdeep singh <radewala.2@gmail.com>
2024-06-08 08:03:52 +0000

    fix the nil_ls rename issue

--- config/plug/lsp/lsp.nix
+++ config/plug/lsp/lsp.nix
@@ -7,7 +7,7 @@
         eslint = {enable = true;};
         html = {enable = true;};
         lua-ls = {enable = true;};
-        nil_ls = {enable = true;};
+        nil-ls = {enable = true;};
         marksman = {enable = true;};
         pyright = {enable = true;};
         gopls = {enable = true;};
