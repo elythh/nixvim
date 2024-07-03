c2a92288e0c8b58bcef4d282490c0d191b37c8eb
Andrew Eggleston <egglestonandrew927@gmail.com>
2024-07-03 14:25:55 -0400

    change color

--- config/plug/colorscheme/colorscheme.nix
+++ config/plug/colorscheme/colorscheme.nix
@@ -5,7 +5,7 @@ in {
     base16 = {
       enable = true;
       setUpBar = false;
-      colorscheme = import ../../colors/${config.theme}.nix {};
+      colorscheme = "woodland";
     };
     gruvbox = {
       enable = false;
