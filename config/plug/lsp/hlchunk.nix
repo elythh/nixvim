39f68134912d07173384dd07f930d70de5adf678
elythh <gwen@omg.lol>
2024-07-07 02:50:22 +0200

    :wrench: (hlchunk): update

--- config/plug/lsp/hlchunk.nix
+++ config/plug/lsp/hlchunk.nix
@@ -5,8 +5,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "shellRaining";
         repo = "hlchunk.nvim";
-        rev = "882d1bc86d459fa8884398223c841fd09ea61b6b";
-        hash = "sha256-fvFvV7KAOo7xtOCjhGS5bDUzwd10DndAKs3++dunED8=";
+        rev = "930fe6262f87d66ca0c85dada3bb7c2a8cb6ec3e";
+        hash = "sha256-IwxyUwL10pop+Aod7BFM67JFXVY37z2Ly4EnlGPwfls=";
       };
     })
   ];
