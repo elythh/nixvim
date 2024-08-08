3900de6298abbc7d72a1d51168ef28b66bbc395f
elythh <gwen@omg.lol>
2024-08-09 00:06:57 +0200

    :wrench: (mini.files): i dont like it, it should open the relative path

--- config/plug/utils/mini.nix
+++ config/plug/utils/mini.nix
@@ -4,25 +4,6 @@
     modules = {
       ai = { };
       surround = { };
-      files = {
-        mappings = {
-          go_in_plus = "<CR>";
-        };
-        windows = {
-          preview = true;
-        };
-      };
     };
   };
-  keymaps = [
-    {
-      mode = "n";
-      key = "-";
-      action = ":lua MiniFiles.open()<CR>";
-      options = {
-        desc = "Open parent directory";
-        silent = true;
-      };
-    }
-  ];
 }
--- config/plug/utils/oil.nix
+++ config/plug/utils/oil.nix
@@ -36,4 +36,15 @@
       };
     };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "-";
+      action = ":Oil<CR>";
+      options = {
+        desc = "Open parent directory";
+        silent = true;
+      };
+    }
+  ];
 }
