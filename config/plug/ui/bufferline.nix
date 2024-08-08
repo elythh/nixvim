44f80ffa7571bbe813d67ee2a89b2f22fc261d5b
elythh <gwen@omg.lol>
2024-08-08 20:14:21 +0200

    :art: (fmt): fix nix flake check
    codes that needed to be formated

--- config/plug/ui/bufferline.nix
+++ config/plug/ui/bufferline.nix
@@ -1,11 +1,12 @@
 { config, lib, ... }:
-let colors = import ../../colors/${config.theme}.nix { };
-in {
+let
+  colors = import ../../colors/${config.theme}.nix { };
+in
+{
   plugins = {
     bufferline = {
       enable = true;
-      separatorStyle =
-        "thick"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
+      separatorStyle = "thick"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
       highlights = lib.mkIf config.colorschemes.base16.enable {
         fill = {
           fg = "none";
@@ -77,42 +78,54 @@ in {
       mode = "n";
       key = "<Tab>";
       action = "<cmd>BufferLineCycleNext<cr>";
-      options = { desc = "Cycle to next buffer"; };
+      options = {
+        desc = "Cycle to next buffer";
+      };
     }
 
     {
       mode = "n";
       key = "<S-Tab>";
       action = "<cmd>BufferLineCyclePrev<cr>";
-      options = { desc = "Cycle to previous buffer"; };
+      options = {
+        desc = "Cycle to previous buffer";
+      };
     }
 
     {
       mode = "n";
       key = "<S-l>";
       action = "<cmd>BufferLineCycleNext<cr>";
-      options = { desc = "Cycle to next buffer"; };
+      options = {
+        desc = "Cycle to next buffer";
+      };
     }
 
     {
       mode = "n";
       key = "<S-h>";
       action = "<cmd>BufferLineCyclePrev<cr>";
-      options = { desc = "Cycle to previous buffer"; };
+      options = {
+        desc = "Cycle to previous buffer";
+      };
     }
 
     {
       mode = "n";
       key = "<leader>bd";
       action = "<cmd>bdelete<cr>";
-      options = { desc = "Delete buffer"; };
+      options = {
+        desc = "Delete buffer";
+      };
     }
 
     {
       mode = "n";
       key = "<leader>bb";
       action = "<cmd>e #<cr>";
-      options = { desc = "Switch to Other Buffer"; };
+      options = {
+        desc = "Switch to Other Buffer";
+      };
     }
 
     # {
@@ -128,35 +141,45 @@ in {
       mode = "n";
       key = "<leader>br";
       action = "<cmd>BufferLineCloseRight<cr>";
-      options = { desc = "Delete buffers to the right"; };
+      options = {
+        desc = "Delete buffers to the right";
+      };
     }
 
     {
       mode = "n";
       key = "<leader>bl";
       action = "<cmd>BufferLineCloseLeft<cr>";
-      options = { desc = "Delete buffers to the left"; };
+      options = {
+        desc = "Delete buffers to the left";
+      };
     }
 
     {
       mode = "n";
       key = "<leader>bo";
       action = "<cmd>BufferLineCloseOthers<cr>";
-      options = { desc = "Delete other buffers"; };
+      options = {
+        desc = "Delete other buffers";
+      };
     }
 
     {
       mode = "n";
       key = "<leader>bp";
       action = "<cmd>BufferLineTogglePin<cr>";
-      options = { desc = "Toggle pin"; };
+      options = {
+        desc = "Toggle pin";
+      };
     }
 
     {
       mode = "n";
       key = "<leader>bP";
       action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
-      options = { desc = "Delete non-pinned buffers"; };
+      options = {
+        desc = "Delete non-pinned buffers";
+      };
     }
   ];
 }
