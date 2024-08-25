468a54737d99ddb8020586cd11b525c959f6fad5
elythh <gwen@omg.lol>
2024-08-25 15:09:26 +0200

    :bug: (conform): use new settings

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -1,62 +1,66 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
-    notifyOnError = true;
-    formattersByFt = {
-      liquidsoap = [ "liquidsoap-prettier" ];
-      html = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      css = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      javascript = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      javascriptreact = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      typescript = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      typescriptreact = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      python = [ "black" ];
-      lua = [ "stylua" ];
-      nix = [ "nixfmt" ];
-      markdown = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      yaml = [
-        "yamllint"
-        "yamlfmt"
-      ];
+    settings = {
+
+      format_on_save = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      notify_on_error = true;
+
+      formattersByFt = {
+        liquidsoap = [ "liquidsoap-prettier" ];
+        html = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        css = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        javascript = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        javascriptreact = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        typescript = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        typescriptreact = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        python = [ "black" ];
+        lua = [ "stylua" ];
+        nix = [ "nixfmt" ];
+        markdown = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        yaml = [
+          "yamllint"
+          "yamlfmt"
+        ];
+      };
     };
   };
 }
