6346b2913220837b799db3a8aa87df5515083864
elythh <gwen@omg.lol>
2024-08-29 14:00:50 +0200

    :construction: (avante): testing

--- config/default.nix
+++ config/default.nix
@@ -8,6 +8,7 @@
 
     ./plug/colorscheme/colorscheme.nix
 
+    ./plug/completion/avante.nix
     ./plug/completion/cmp.nix
     ./plug/completion/codecompanion.nix
     ./plug/completion/copilot-cmp.nix
