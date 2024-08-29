e6801551f81baa12983dc19266edb77745fb6b4f
elythh <gwen@omg.lol>
2024-08-29 13:03:58 +0200

    :wrench: (comment-box): use nixvim
    thanks @elythh

--- config/plug/utils/comment-box.nix
+++ config/plug/utils/comment-box.nix
@@ -1,18 +1,5 @@
-{ pkgs, ... }:
-let
-  version = "v1.0.2";
-in
 {
-  extraPlugins = with pkgs.vimUtils; [
-    (buildVimPlugin {
-      pname = "comment-box.nvim";
-      inherit version;
-      src = pkgs.fetchFromGitHub {
-        owner = "LudoPinelli";
-        repo = "comment-box.nvim";
-        rev = "refs/tags/${version}";
-        hash = "sha256-2P8Zyd5ucOvihZdjgSmr7YlV0yuCn+LyHkCj9gPx/CY=";
-      };
-    })
-  ];
+  plugins.comment-box = {
+    enable = true;
+  };
 }
