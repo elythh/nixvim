2638660f47a70e325471bb99cc3714cf8da44d1c
elyth <gwen@omg.lol>
2024-09-07 00:21:27 +0200

    :wrench: (markview): use nixvim

--- config/plug/utils/markview.nix
+++ config/plug/utils/markview.nix
@@ -1,19 +1,5 @@
-{ pkgs, ... }:
 {
-  extraPlugins = with pkgs.vimUtils; [
-    (buildVimPlugin rec {
-      pname = "markview.nvim";
-      version = "1.0.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "OXY2DEV";
-        repo = "markview.nvim";
-        rev = "refs/tags/v${version}";
-        hash = "sha256-mhRg/cszW/3oXdC1yvbpCeqWQA9WLW5FvcqGd/wBTnE=";
-      };
-    })
-  ];
-
-  extraConfigLua = ''
-    require("markview").setup();
-  '';
+  plugins.markview = {
+    enable = true;
+  };
 }
