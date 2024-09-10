cbd0302a99a2e9313c80c95cc743040aa3f5f111
elythh <gwenchlan@tuta.io>
2024-09-10 18:03:27 +0200

    :fire: (copilotchat): fuck you

--- config/default.nix
+++ config/default.nix
@@ -48,7 +48,6 @@
     ./plug/utils/harpoon.nix
     ./plug/utils/comment.nix
     ./plug/utils/comment-box.nix
-    ./plug/utils/copilot.nix
     ./plug/utils/flash.nix
     ./plug/utils/grapple.nix
     ./plug/utils/hardtime.nix
