ee1445642781ebf4cab5518f3f64497d0008aaea
elyth <gwen@omg.lol>
2024-09-10 23:31:42 +0200

    :wrench: (autocmd): highlight yank and enter git message in insert mode

--- config/autocommands.nix
+++ config/autocommands.nix
@@ -30,5 +30,21 @@
       ];
       command = "setlocal spell spelllang=en,fr";
     }
+    # Hilight yank text
+    {
+      event = "TextYankPost";
+      pattern = "*";
+      command = "lua vim.highlight.on_yank{timeout=500}";
+    }
+    # Enter git buffer in insert mode
+    {
+      event = "FileType";
+      pattern = [
+        "gitcommit"
+        "gitrebase"
+      ];
+      command = "startinsert | 1";
+    }
+
   ];
 }
