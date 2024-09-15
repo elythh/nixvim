0ad3c50610a6746aa8a8debd5408b4d4d837961e
elyth <gwen@omg.lol>
2024-09-15 14:39:28 +0200

    :sparkles: (mini): indentscope

--- config/plug/utils/mini.nix
+++ config/plug/utils/mini.nix
@@ -9,6 +9,17 @@
       surround = { };
       tabline = { };
       trailspace = { };
+      indentscope = {
+        symbol = "";
+        # draw = {
+        #   delay = 100;
+        #   priority = 2;
+        #   animation.__raw = ''
+        #     function(s, n)
+        #       return s/n*20
+        #     end'';
+        # };
+      };
     };
   };
 }
