e71fb6a54810e9349c25712d5329ecfb657e46e7
elyth <gwen@omg.lol>
2024-10-03 15:47:44 +0200

    :wrench: (lsp): rework keybind
    use lspsaga when I can

--- config/plug/lsp/lsp.nix
+++ config/plug/lsp/lsp.nix
@@ -58,33 +58,34 @@
             action = "type_definition";
             desc = "Type Definition";
           };
-          K = {
-            action = "hover";
-            desc = "Hover";
-          };
-          "<leader>cw" = {
-            action = "workspace_symbol";
-            desc = "Workspace Symbol";
-          };
+          # Use LSP saga keybinding instead
+          # K = {
+          #   action = "hover";
+          #   desc = "Hover";
+          # };
+          # "<leader>cw" = {
+          #   action = "workspace_symbol";
+          #   desc = "Workspace Symbol";
+          # };
           "<leader>cr" = {
             action = "rename";
             desc = "Rename";
           };
         };
-        diagnostic = {
-          "<leader>cd" = {
-            action = "open_float";
-            desc = "Line Diagnostics";
-          };
-          "[d" = {
-            action = "goto_next";
-            desc = "Next Diagnostic";
-          };
-          "]d" = {
-            action = "goto_prev";
-            desc = "Previous Diagnostic";
-          };
-        };
+        # diagnostic = {
+        #   "<leader>cd" = {
+        #     action = "open_float";
+        #     desc = "Line Diagnostics";
+        #   };
+        #   "[d" = {
+        #     action = "goto_next";
+        #     desc = "Next Diagnostic";
+        #   };
+        #   "]d" = {
+        #     action = "goto_prev";
+        #     desc = "Previous Diagnostic";
+        #   };
+        # };
       };
     };
   };
