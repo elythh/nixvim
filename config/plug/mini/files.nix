f178b48cbc183140a30efc8b254165742228d1e4
elyth <gwen@omg.lol>
2024-12-01 03:20:18 +0100

    :wrench: (mini.files): some cool keybinds in found in @linkarzu's video
    https://www.youtube.com/watch?v=BzblG2eV8dU

--- config/plug/mini/files.nix
+++ config/plug/mini/files.nix
@@ -11,9 +11,9 @@
       preview = true;
       width_preview = 100;
     };
-    mappings =
-      {
-      };
+    mappings = {
+      synchronize = "s";
+    };
   };
   keymaps = lib.mkIf (config.plugins.mini.enable && lib.hasAttr "files" config.plugins.mini.modules) [
     {
@@ -34,6 +34,50 @@
         silent = true;
       };
     }
+    {
+      mode = "n";
+      key = "<M-c>";
+      action.__raw = ''
+        function()
+          local curr_entry = MiniFiles.get_fs_entry()
+          if curr_entry then
+            -- Convert path to be relative to home directory
+            local home_dir = vim.fn.expand("~")
+            local relative_path = curr_entry.path:gsub("^" .. home_dir, "~")
+            vim.fn.setreg("+", relative_path) -- Copy the relative path to the clipboard register
+            vim.notify(relative_path, vim.log.levels.INFO)
+            vim.notify("Path copied to clipboard: ", vim.log.levels.INFO)
+          else
+            vim.notify("No file or directory selected", vim.log.levels.WARN)
+          end
+        end
+      '';
+    }
+    {
+      mode = "n";
+      key = "<M-C>";
+      action.__raw = ''
+        function()
+              -- Get the current entry (file or directory)
+           local curr_entry = MiniFiles.get_fs_entry()
+           if curr_entry then
+             local path = curr_entry.path
+             -- Escape the path for shell command
+             local escaped_path = vim.fn.fnameescape(path)
+             -- Build the osascript command to copy the file or directory to the clipboard
+             local cmd = string.format([[cat %s | wl-copy ]], escaped_path)
+             local result = vim.fn.system(cmd)
+             if vim.v.shell_error ~= 0 then
+               vim.notify("Copy failed: " .. result, vim.log.levels.ERROR)
+             else
+               vim.notify(path, vim.log.levels.INFO)
+               vim.notify("Copied to system clipboard", vim.log.levels.INFO)
+             end
+           else
+             vim.notify("No file or directory selected", vim.log.levels.WARN)
+           end
+        end
+      '';
+    }
   ];
-
 }
