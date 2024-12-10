8139737358993d5233d01bc1a55641c79b51f003
elyth <gwen@omg.lol>
2024-12-10 01:14:40 +0100

    :wrench: (telescope): refactor keymappings
    thanks TJ

--- config/plug/ui/telescope.nix
+++ config/plug/ui/telescope.nix
@@ -1,26 +1,201 @@
 {
-  lib,
   config,
+  lib,
   pkgs,
   ...
 }:
 {
+  extraPackages = with pkgs; [ ripgrep ];
+
+  keymaps = lib.mkIf config.plugins.telescope.enable [
+    {
+      mode = "n";
+      key = "<leader>fF";
+      action.__raw = ''
+        function()
+          require("telescope.builtin").find_files({ hidden = true, no_ignore = true})
+        end
+      '';
+      options = {
+        desc = "Find all files";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fW";
+      action.__raw = ''
+        function()
+          require("telescope.builtin").live_grep {
+            additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+          }
+        end
+      '';
+      options = {
+        desc = "Find words in all files";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>f?";
+      action.__raw = ''
+        function()
+          require("telescope.builtin").live_grep { grep_open_files=true }
+        end
+      '';
+      options = {
+        desc = "Find words in all open buffers";
+        silent = true;
+      };
+    }
+    (lib.mkIf config.plugins.telescope.extensions.undo.enable {
+      mode = "n";
+      key = "<leader>fu";
+      action = "<cmd>Telescope undo<CR>";
+      options = {
+        desc = "List undo history";
+      };
+    })
+    (lib.mkIf config.plugins.telescope.extensions.manix.enable {
+      mode = "n";
+      key = "<leader>fM";
+      action = "<cmd>Telescope manix<CR>";
+      options = {
+        desc = "Search manix";
+      };
+    })
+  ];
+
   plugins.telescope = {
     enable = true;
+
     extensions = {
       file-browser = {
         enable = true;
+        settings = {
+          hidden = true;
+        };
       };
-      ui-select = {
-        enable = true;
-      };
+
       frecency = {
-        enable = true;
+        # FIXME: super slow loading
+        # enable = true;
+        settings = {
+          auto_validate = false;
+        };
       };
+
       fzf-native = {
         enable = true;
       };
+
+      live-grep-args.enable = true;
+
+      manix.enable = true;
+
+      ui-select = {
+        enable = true;
+        settings = {
+          __unkeyed.__raw = ''require("telescope.themes").get_dropdown{}'';
+        };
+      };
+
+      undo = {
+        enable = true;
+        settings = {
+          side_by_side = true;
+          layout_strategy = "vertical";
+          layout_config = {
+            preview_height = 0.8;
+          };
+        };
+      };
     };
+
+    keymaps = {
+      "<leader>f'" = {
+        action = "marks";
+        options.desc = "View marks";
+      };
+      "<leader>f/" = {
+        action = "current_buffer_fuzzy_find";
+        options.desc = "Fuzzy find in current buffer";
+      };
+      "<leader>f<CR>" = {
+        action = "resume";
+        options.desc = "Resume action";
+      };
+      "<leader>fa" = {
+        action = "autocommands";
+        options.desc = "View autocommands";
+      };
+      "<leader>fC" = {
+        action = "commands";
+        options.desc = "View commands";
+      };
+      "<leader>fb" = {
+        action = "buffers";
+        options.desc = "View buffers";
+      };
+      "<leader>fc" = {
+        action = "grep_string";
+        options.desc = "Grep string";
+      };
+      "<leader>fd" = {
+        action = "diagnostics";
+        options.desc = "View diagnostics";
+      };
+      "<leader>ff" = {
+        action = "find_files";
+        options.desc = "Find files";
+      };
+      "<leader><leader>" = {
+        action = "find_files";
+        options.desc = "Find files";
+      };
+      "<leader>fh" = {
+        action = "help_tags";
+        options.desc = "View help tags";
+      };
+      "<leader>fm" = {
+        action = "man_pages";
+        options.desc = "View man pages";
+      };
+      "<leader>fo" = {
+        action = "oldfiles";
+        options.desc = "View old files";
+      };
+      "<leader>fr" = {
+        action = "registers";
+        options.desc = "View registers";
+      };
+      "<leader>fs" = {
+        action = "lsp_document_symbols";
+        options.desc = "Search symbols";
+      };
+      "<leader>fq" = {
+        action = "quickfix";
+        options.desc = "Search quickfix";
+      };
+      "<leader>gB" = {
+        action = "git_branches";
+        options.desc = "View git branches";
+      };
+      "<leader>gC" = {
+        action = "git_commits";
+        options.desc = "View git commits";
+      };
+      "<leader>gs" = {
+        action = "git_status";
+        options.desc = "View git status";
+      };
+      "<leader>gS" = {
+        action = "git_stash";
+        options.desc = "View git stashes";
+      };
+    };
+
     settings = {
       defaults = {
         vimgrep_arguments = [
@@ -43,193 +218,22 @@
           };
         };
         sorting_strategy = "ascending";
+        file_ignore_patterns = [
+          "^.git/"
+          "^.mypy_cache/"
+          "^__pycache__/"
+          "^output/"
+          "^data/"
+          "%.ipynb"
+        ];
+        set_env.COLORTERM = "truecolor";
       };
+
       pickers = {
         colorscheme = {
           enable_preview = true;
         };
       };
-      mappings = {
-        # This is not working, use Alt + the key instead 🤷
-        n = {
-          "d" = {
-            __raw = "require('telescope.actions').delete_buffer";
-          };
-          "q" = {
-            __raw = "require('telescope.actions').close";
-          };
-        };
-      };
-    };
-    keymaps = {
-      "<leader><space>" = {
-        action = "find_files";
-        options = {
-          desc = "Find project files";
-        };
-      };
-      "<leader>:" = {
-        action = "command_history";
-        options = {
-          desc = "Command History";
-        };
-      };
-      "<leader>b" = {
-        action = "buffers";
-        options = {
-          desc = "+buffer";
-        };
-      };
-      # "<leader>ff" = {
-      #   action = "find_files";
-      #   options = {
-      #     desc = "Find project files";
-      #   };
-      # };
-      "<leader>fr" = {
-        action = "live_grep";
-        options = {
-          desc = "Find text";
-        };
-      };
-      "<leader>fe" = {
-        action = "resume";
-        options = {
-          desc = "Resume";
-        };
-      };
-      "<leader>fg" = {
-        action = "oldfiles";
-        options = {
-          desc = "Recent";
-        };
-      };
-      "<leader>fb" = {
-        action = "buffers";
-        options = {
-          desc = "Buffers";
-        };
-      };
-      "<C-p>" = {
-        action = "git_files";
-        options = {
-          desc = "Search git files";
-        };
-      };
-      "<leader>gc" = {
-        action = "git_commits";
-        options = {
-          desc = "Commits";
-        };
-      };
-      "<leader>gs" = {
-        action = "git_status";
-        options = {
-          desc = "Status";
-        };
-      };
-      "<leader>sa" = {
-        action = "autocommands";
-        options = {
-          desc = "Auto Commands";
-        };
-      };
-      "<leader>sb" = {
-        action = "current_buffer_fuzzy_find";
-        options = {
-          desc = "Buffer";
-        };
-      };
-      "<leader>sc" = {
-        action = "command_history";
-        options = {
-          desc = "Command History";
-        };
-      };
-      "<leader>sC" = {
-        action = "commands";
-        options = {
-          desc = "Commands";
-        };
-      };
-      "<leader>sD" = {
-        action = "diagnostics";
-        options = {
-          desc = "Workspace diagnostics";
-        };
-      };
-      "<leader>sh" = {
-        action = "help_tags";
-        options = {
-          desc = "Help pages";
-        };
-      };
-      "<leader>sH" = {
-        action = "highlights";
-        options = {
-          desc = "Search Highlight Groups";
-        };
-      };
-      "<leader>sk" = {
-        action = "keymaps";
-        options = {
-          desc = "Keymaps";
-        };
-      };
-      "<leader>sM" = {
-        action = "man_pages";
-        options = {
-          desc = "Man pages";
-        };
-      };
-      "<leader>sm" = {
-        action = "marks";
-        options = {
-          desc = "Jump to Mark";
-        };
-      };
-      "<leader>so" = {
-        action = "vim_options";
-        options = {
-          desc = "Options";
-        };
-      };
-      "<leader>uC" = {
-        action = "colorscheme";
-        options = {
-          desc = "Colorscheme preview";
-        };
-      };
     };
   };
-  keymaps = lib.mkIf config.plugins.telescope.enable [
-    {
-      mode = "n";
-      key = "<leader>sd";
-      action = "<cmd>Telescope diagnostics bufnr=0<cr>";
-      options = {
-        desc = "Document diagnostics";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>ff";
-      action.__raw = ''
-        function()
-            require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h") })
-          end
-      '';
-      options.desc = "Search file cwd";
-    }
-    {
-      mode = "n";
-      key = "<leader>fR";
-      action.__raw = ''
-        function()
-           require("telescope.builtin").live_grep({ cwd = vim.fn.expand("%:p:h") })
-          end
-      '';
-      options.desc = "Grep cwd";
-    }
-  ];
 }
