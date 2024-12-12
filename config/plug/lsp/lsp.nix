7ab489ced09c2fa9834f5a1f0928d57e694580c0
elyth <gwen@omg.lol>
2024-12-12 14:51:46 +0100

    :wrench: (lsp): configure nixd

--- config/plug/lsp/lsp.nix
+++ config/plug/lsp/lsp.nix
@@ -1,4 +1,9 @@
 {
+  lib,
+  pkgs,
+  ...
+}:
+{
   plugins = {
     lsp-format = {
       enable = true;
@@ -13,19 +18,24 @@
         lua_ls = {
           enable = true;
         };
-        # nil-ls = {
-        #   enable = true;
-        # };
         nixd = {
           enable = true;
-          extraOptions = {
-            nixos = {
-              expr = "(builtins.getFlake \"/etc/nixos\").nixosConfigurations.aurelionite.options";
+          settings =
+            let
+              flake = ''(builtins.getFlake "github:elythh/flake)""'';
+            in
+            {
+              nixpkgs = {
+                expr = "import ${flake}.inputs.nixpkgs { }";
+              };
+              formatting = {
+                command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+              };
+              options = {
+                nixos.expr = ''${flake}.nixosConfigurations.grovetender.options'';
+                nixvim.expr = ''${flake}.packages.${pkgs.system}.nvim.options'';
+              };
             };
-            home_manager = {
-              expr = "(builtins.getFlake \"/etc/nixos\").homeConfigurations.aurelionite.options";
-            };
-          };
         };
         markdown_oxide = {
           enable = true;
