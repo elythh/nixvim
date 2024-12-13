200f7579852ff76aef8884cd65e3cb32376623ce
elythh <gwenchlan@tuta.io>
2024-12-13 16:19:19 +0100

    :art: change to paradise

--- config/default.nix
+++ config/default.nix
@@ -86,7 +86,7 @@
   };
   config = {
     # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
-    theme = "far";
+    theme = "paradise";
     extraConfigLua = ''
       _G.theme = "${config.theme}"
     '';
