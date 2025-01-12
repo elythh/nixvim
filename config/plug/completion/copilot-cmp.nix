c7f69c6ae4cb7c28f2a280e3e7546dbf2887bcf2
elyth <gwen@omg.lol>
2025-01-12 12:50:14 +0100

    :wrench: (copilot-cmp): rename options

--- config/plug/completion/copilot-cmp.nix
+++ config/plug/completion/copilot-cmp.nix
@@ -5,11 +5,13 @@
   };
   plugins.copilot-lua = {
     enable = true;
-    suggestion = {
-      enabled = false;
-    };
-    panel = {
-      enabled = false;
+    settings = {
+      suggestion = {
+        enabled = false;
+      };
+      panel = {
+        enabled = false;
+      };
     };
   };
 
