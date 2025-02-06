a1c876fa156a2380348220a275946655594c7bc4
elyth <gwen@omg.lol>
2025-02-06 23:36:48 +0100

    :wrench: (notify): change style (unused)

--- modules/nixvim/plug/nvim-notify/default.nix
+++ modules/nixvim/plug/nvim-notify/default.nix
@@ -2,11 +2,13 @@
 {
   plugins.notify = {
     enable = false;
-    backgroundColour = "#000000";
-    fps = 60;
-    render = "default";
-    timeout = 1000;
-    topDown = true;
+    settings = {
+      backgroundColour = "#000000";
+      fps = 60;
+      render = "default";
+      timeout = 1000;
+      topDown = true;
+    };
   };
   keymaps = lib.mkIf config.plugins.notify.enable [
     {
