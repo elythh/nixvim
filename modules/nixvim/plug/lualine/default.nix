50ff36e676c95a8c65416db900876c4c4c21f3df
elyth <gwen@omg.lol>
2025-02-07 02:32:50 +0100

    :art: (lualine): style

--- modules/nixvim/plug/lualine/default.nix
+++ modules/nixvim/plug/lualine/default.nix
@@ -8,6 +8,11 @@ in
     lazyLoad.settings.event = "BufEnter";
     settings = {
       options = {
+        component_separators = "";
+        section_separators = {
+          left = "";
+          right = "";
+        };
         theme = {
           normal = {
             a = {
@@ -40,7 +45,6 @@ in
       inactive_sections = {
         lualine_x = [
           "filename"
-          "filetype"
         ];
       };
       sections = {
@@ -55,25 +59,52 @@ in
             separator.left = "";
             separator.right = "";
           }
-        ];
-        lualine_b = [
           {
             __unkeyed = "branch";
             icon.__unkeyed = "";
             color = {
               fg = colors.base04;
-              bg = "nil";
             };
             separator.left = "";
             separator.right = "";
           }
+          {
+            __unkeyed = {
+              __raw = ''
+                function()
+                    local msg = ""
+                    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                    local clients = vim.lsp.get_active_clients()
+                    if next(clients) == nil then
+                        return msg
+                    end
+                    for _, client in ipairs(clients) do
+                        local filetypes = client.config.filetypes
+                        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                            return client.name
+                        end
+                    end
+                    return msg
+                end
+              '';
+            };
+            icon.__unkeyed = "";
+            color = {
+              fg = colors.base0B;
+            };
+          }
+        ];
+        lualine_b = [
           {
             __unkeyed = "diff";
             separator.left = "";
             separator.right = "";
           }
         ];
-        lualine_c = [
+        lualine_c = [ "" ];
+        lualine_x = [ "" ];
+        lualine_y = [ "" ];
+        lualine_z = [
           {
             __unkeyed = "diagnostic";
             symbols = {
@@ -89,16 +120,39 @@ in
             separator.left = "";
             separator.right = "";
           }
-        ];
-        lualine_x = [ "" ];
-        lualine_y = [ "" ];
-        lualine_z = [
           {
             __unkeyed = "location";
             color = {
-              fg = colors.base0B;
-              bg = "nil";
+              fg = colors.base0D;
             };
+            separator.left = "";
+            separator.right = "";
+          }
+          {
+            __unkeyed = {
+              __raw = ''
+                function()
+                  local chars = setmetatable({
+                    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+                    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+                  }, { __index = function() return " " end })
+                  local line_ratio = vim.api.nvim_win_get_cursor(0)[1] / vim.api.nvim_buf_line_count(0)
+                  local position = math.floor(line_ratio * 100)
+
+                  local icon = chars[math.floor(line_ratio * #chars)] .. position
+                  if position <= 5 then
+                    icon = " TOP"
+                  elseif position >= 95 then
+                    icon = " BOT"
+                  end
+                  return icon
+                end
+              '';
+            };
+            color = {
+              fg = colors.base0C;
+            };
+            padding = 0;
             separator.left = "";
             separator.right = "";
           }
