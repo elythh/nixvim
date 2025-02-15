aac0783dba2ca0c5ebfe2f2969d6d4fcd239d6c4
elyth <gwen@omg.lol>
2025-02-15 20:27:28 +0100

    feat: keymapping

--- config/keys.nix
+++ config/keys.nix
@@ -1,5 +1,79 @@
 {
   config = {
     globals.mapleader = " ";
+
+    keymaps = [
+      # Clear and replace word under cursor
+      {
+        mode = "n";
+        key = "<C-c>";
+        action = "ciw";
+        options = {
+          desc = "Clear and replace word under cursor";
+        };
+      }
+
+      # Search and replace word under cursor
+      {
+        mode = "n";
+        key = "<leader>R";
+        action = ":%s/\<C-r><C-w>//g<Left><Left>";
+        options = {
+          desc = "Search and replace word under cursor";
+        };
+      }
+
+      # Disable search highlighting
+      {
+        mode = "n";
+        key = "<leader>/";
+        action = "<cmd>nohl<CR>";
+        options = {
+          desc = "Clear search";
+        };
+      }
+
+      # Move lines in visual mode
+      {
+        mode = "v";
+        key = "J";
+        action = ":m '>+1<CR>gv=gv";
+        options = {
+          desc = "Use move command when line is highlighted ";
+        };
+      }
+      {
+        mode = "v";
+        key = "K";
+        action = ":m '>-2<CR>gv=gv";
+        options = {
+          desc = "Use move command when line is highlighted ";
+        };
+      }
+
+      # Void register paste
+      {
+        mode = "x";
+        key = "<leader>p";
+        action = ''"_dP'';
+        options = {
+          desc = "Deletes to void register and paste over";
+        };
+      }
+
+      # Delete to void register
+      {
+        mode = [
+          "n"
+          "v"
+        ];
+        key = "<leader>D";
+        action = ''"_d'';
+        options = {
+          desc = "Delete to void register";
+        };
+      }
+
+    ];
   };
 }
