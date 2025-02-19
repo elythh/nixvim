bdae2522e8249f52ec044193a4533a54ab9bd808
elythh <gwenchlan@tuta.io>
2025-02-19 13:47:36 +0100

    feat: add markview
    Markdown rendering plugin

---
+++ config/plugins/markview/default.nix
@@ -0,0 +1,23 @@
+{
+  plugins.markview = {
+    enable = true;
+    settings = {
+      buf_ignore = [ ];
+      hybrid_modes = [
+        "i"
+      ];
+      modes = [
+        "n"
+        "i"
+        "no"
+        "c"
+      ];
+      callbacks.on_enable.__raw = ''
+        function(_, win)
+          vim.wo[win].conceallevel = 2
+          vim.wo[win].concealcursor = "nc"
+        end
+      '';
+    };
+  };
+}
